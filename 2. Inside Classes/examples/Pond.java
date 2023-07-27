0d0ed7c89ffb0f50bdc198780dfc6d7347dc0f80
Xeniorita Alondra Bio <Xenioritaalondrabio@gmail.com>
2023-07-27 21:06:01 +0800

    Exercises

--- 2. Inside Classes/examples/Pond.java
+++ 2. Inside Classes/examples/Pond.java
@@ -21,7 +21,7 @@ public class Pond {
 		System.out.println(mobyDuck);
 		
 		// Exercise: Make the Duck quack
-		
+		wolfgangDuck.quack();
 	}
 
 }
--- 2. Inside Classes/exercises/Cat.java
+++ 2. Inside Classes/exercises/Cat.java
@@ -31,16 +31,14 @@ public class Cat {
 	}
 
 	public static void main(String[] args) {
+
 		/* Do the following things without changing the Cat class */
-
+		Cat cat = new Cat("Chino");
 		// 1. Make the Cat meow
-		
+		cat.meow();
 		// 2. Get the Cat to print it's name
-
+		cat.printName();
 		// 3. Kill the Cat!
 
 	}
 }
-
-
-
